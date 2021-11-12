588e74d371f7158a81551fa3cd46bf93738a0e51
Jose <joblari92@gmail.com>
2021-11-12 18:21:23 +0100

    Configuración del método viewBinding

--- app/src/main/java/com/example/garabatugolfclub/MainActivity.kt
+++ app/src/main/java/com/example/garabatugolfclub/MainActivity.kt
@@ -2,19 +2,30 @@ package com.example.garabatugolfclub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.garabatugolfclub.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        Thread.sleep(2000)
+        //Thread.sleep(2000)
         /**
-         * Dejamos un tiempo para que se muestre
-         * la Splashscreen antes de que se inicie la aplicación
-         * y después cambiamos al tema principal
+         * Dejamos un tiempo, aunque puede que
+         * no sea necesario,para que se muestre
+         * la Splashscreen antes de que se inicie
+         * la aplicación y después cambiamos
+         * al tema principal
          * */
         setTheme(R.style.Theme_GarabatuGolfClub)
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        /**
+         * Utilizaremos el método viewBinding tanto en esta activity como
+         * en todas las que vayamos a crear.
+         */
     }
 }
