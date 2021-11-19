7b29b9ef8889e63089c31525338878aebbab4524
Jose <joblari92@gmail.com>
2021-11-19 17:52:56 +0100

    com.google.firebase.FirebaseException: An internal error has occurred. [ socket failed:EPERM (Operation not permitted) ]

--- app/src/main/java/com/example/garabatugolfclub/MainActivity.kt
+++ app/src/main/java/com/example/garabatugolfclub/MainActivity.kt
@@ -9,6 +9,9 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.example.garabatugolfclub.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -22,7 +25,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         Thread.sleep(2000)
-        /**
+        /*
          * Dejamos un tiempo, aunque puede que
          * no sea necesario,para que se muestre
          * la Splashscreen antes de que se inicie
@@ -35,17 +38,65 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        /**
+        /*
          * Utilizaremos el método viewBinding tanto en esta activity como
          * en todas las que vayamos a crear.
          */
 
-        //Setup
-        //setup()
+        // Inicializamos Firebase Auth
+        auth = Firebase.auth
+
+        binding.registro.setOnClickListener {
+            if (binding.email.text.contains("@") && binding.password.text.isNotEmpty()){
+                createAccount(binding.email.text.toString(),binding.password.text.toString())
+            }else{
+                Toast.makeText(baseContext,"Datos de registro no válidos",Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
 
         //generateFBKey() //Utilizamos esta función para obtener el hash que nos
                         //permita iniciar sesión con Facebook
     }
+
+
+    private fun reload() {
+        TODO()
+    }
+
+    /*Comprobamos si el usuario ya ha iniciado sesión, de ser así
+         *cargamos la siguiente activity con la función reload*/
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    /*Función de registro de nuevos usuarios*/
+    private fun createAccount(email: String, password: String) {
+
+        auth.createUserWithEmailAndPassword(email,
+        password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("reg.exi.", "signInWithEmail:success")
+                    val user = auth.currentUser
+                    Toast.makeText(baseContext, "Usuario registrado",
+                        Toast.LENGTH_SHORT).show()
+                    reload()
+                } else {
+                    Log.w("fail.reg", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+
+    }
+
+
 
     /*fun generateFBKey () {
         try {
@@ -63,52 +114,4 @@ class MainActivity : AppCompatActivity() {
         }
     }*/
 
-    /*private fun setup() {
-
-        title = "Autenticación"
-
-        binding.registro.setOnClickListener {
-
-            if (binding.email.text.isNotEmpty() && binding.password.text.isNotEmpty()) {
-
-                FirebaseAuth.getInstance()
-                    .createUserWithEmailAndPassword(binding.email.text.toString(),
-                        binding.password.text.toString()).addOnCompleteListener {
-                            if (it.isSuccessful) {
-                                showInicio(it.result?.user?.email ?:"")
-                            } else {
-                                showAlert()
-                            }
-                    }
-            } else {
-
-                val builder = AlertDialog.Builder(this)
-                builder.setTitle("Error")
-                builder.setMessage("Primer if")
-                builder.setPositiveButton("Aceptar", null)
-                val dialog: AlertDialog = builder.create()
-                dialog.show()
-
-            }
-        }
-    }*/
-
-    /*Función para crear mensaje de alerta en caso de
-      no poder iniciar sesión */
-    private fun showAlert() {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Error")
-        builder.setMessage("Se ha producido un error autenticando al usuario")
-        builder.setPositiveButton("Aceptar", null)
-        val dialog: AlertDialog = builder.create()
-        dialog.show()
-    }
-
-    /*Función para lanzar la siguiente actividad*/
-   /* private fun showInicio(email: String) {
-        val inicioIntent = Intent(this, Inicio::class.java).apply {
-            putExtra("email", email)
-        }
-        startActivity(inicioIntent)
-    }*/
 }
