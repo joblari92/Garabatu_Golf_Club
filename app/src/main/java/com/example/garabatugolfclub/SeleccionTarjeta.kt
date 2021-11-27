31c046b9b27b36c018a291d3b20c706030d3f51d
Jose <joblari92@gmail.com>
2021-11-27 20:12:17 +0100

    Dropdown selección de campos

--- app/src/main/java/com/example/garabatugolfclub/SeleccionTarjeta.kt
+++ app/src/main/java/com/example/garabatugolfclub/SeleccionTarjeta.kt
@@ -3,13 +3,13 @@ package com.example.garabatugolfclub
 import android.app.Dialog
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
-import android.widget.Button
-import android.widget.RadioButton
-import android.widget.RadioGroup
-import android.widget.Toast
+import android.widget.*
+import androidx.annotation.MenuRes
 import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.example.garabatugolfclub.databinding.ActivitySeleccionTarjetaBinding
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.firestore.QuerySnapshot
@@ -33,20 +33,13 @@ class SeleccionTarjeta : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        val db = Firebase.firestore
+        //Desplegable en el que seleccionaremos el campo en el que vamos a jugar
+        val campos = resources.getStringArray(R.array.campos)
+        val arrayAdapter = ArrayAdapter(this,R.layout.dropdown_item,campos)
+        binding.autoCompleteTextView.setAdapter(arrayAdapter)
 
-        db.collection("campos").get().addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                val list: MutableList<String> =
-                    ArrayList()
-                for (document in task.result) {
-                    list.add(document.id)
-                }
-                Log.d("listCampos", list.toString())
-            } else {
-                Log.d("listCampos", "Error getting documents: ", task.exception)
-            }
-        }
+        //Variable para almacenar el campo y poder conseguir sus datos de la BBDD
+        val campoSeleccionado = binding.autoCompleteTextView.text
 
 
     }
