51933bfe7184bae027b15411b430c39aa7000942
Jose <joblari92@gmail.com>
2021-11-30 16:45:36 +0100

    Activity de tarjeta simple completada

--- app/src/main/java/com/example/garabatugolfclub/TarjetaSimple.kt
+++ app/src/main/java/com/example/garabatugolfclub/TarjetaSimple.kt
@@ -1,33 +1,27 @@
 package com.example.garabatugolfclub
 
-import android.app.Dialog
 import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Telephony
-import android.util.Log
 import android.widget.Toast
-import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AlertDialog
-import com.example.garabatugolfclub.databinding.ActivitySeleccionTarjetaBinding
 import com.example.garabatugolfclub.databinding.ActivityTarjetaSimpleBinding
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
-import com.google.protobuf.Empty
 
 class TarjetaSimple : AppCompatActivity() {
     private lateinit var binding: ActivityTarjetaSimpleBinding
+
+    //Variable para evitar salir de la activity directamente
+    private var backPressedTime = 0L
 
     //Variables
     val usuario = Firebase.auth.currentUser?.email.toString()
     val db = FirebaseFirestore.getInstance()
     val partido = Partidos(usuario)
     var hoyo = 1
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_GarabatuGolfClub)
@@ -96,13 +90,44 @@ class TarjetaSimple : AppCompatActivity() {
             }
         }
 
+        /*Al pulsar botón cancelar
+    * volveremos a la activity SeleccionTarjeta y eliminaremos los datos del partido
+    * que estábamos jugando, para no tener registros incompletos en la base de datos.*/
         binding.botonCancelar.setOnClickListener {
-            TODO("Salir del partido en curso y eliminar el documento de la BBDD. Utilizar y AlertDialog" +
-                    "para confirmar antes de salir")
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("SALIR")
+            builder.setMessage("¿Seguro que desea salir del partido?")
+            builder.setPositiveButton("ACEPTAR",DialogInterface.OnClickListener{
+                    dialog,which->
+                if (idPartido != null) {
+                    db.collection("usuarios").document(usuario)
+                        .collection("partidos").document(idPartido).delete()
+                }
+                val i = Intent(this, SeleccionTarjeta::class.java)
+                startActivity(i)
+
+            })
+            builder.setNegativeButton("CANCELAR",null)
+            builder.show()
         }
 
+    }
 
-
+    /*Al pulsar dos veces seguidas en menos de dos segundos el botón atrás
+    * volveremos a la activity de selección de pantalla y eliminaremos los datos del partido
+    * que estábamos jugando, para no tener registros incompletos en la base de datos.*/
+    override fun onBackPressed() {
+        val idPressedBack = intent.getStringExtra("idPartido")
+        if(backPressedTime + 2000 > System.currentTimeMillis()){
+            super.onBackPressed()
+            if (idPressedBack != null) {
+                db.collection("usuarios").document(usuario)
+                    .collection("partidos").document(idPressedBack).delete()
+            }
+        }else{
+            Toast.makeText(this,"Pulsa atrás otra vez para salir", Toast.LENGTH_SHORT).show()
+        }
+        backPressedTime = System.currentTimeMillis()
     }
 
 }
